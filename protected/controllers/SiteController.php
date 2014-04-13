7697b24b029d017d833872164ce9eec935acefa3
choirudin <choirudin22@gmail.com>
2014-04-13 17:55:11 +0700

    --uji coba routing

--- protected/controllers/SiteController.php
+++ protected/controllers/SiteController.php
@@ -29,7 +29,10 @@ class SiteController extends Controller
 	{
 		// renders the view file 'protected/views/site/index.php'
 		// using the default layout 'protected/views/layouts/main.php'
-		$this->render('index');
+		//$this->render('index');
+		$model=new LoginForm;
+		$this->render('login',array('model'=>$model));
+
 	}
 
 	/**
