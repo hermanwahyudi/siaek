d252acdac8af8a2e9f7977e890225686f9d7b39a
choirudin <choirudin22@gmail.com>
2014-04-21 21:27:23 +0700

    --add model Absensi dan Feedback

--- protected/models/Feedback.php
+++ protected/models/Feedback.php
@@ -1,5 +1,102 @@
-<?php 
-	class Feedback extends CActiveRecord {
-	
+<?php
+
+/**
+ * This is the model class for table "feedback".
+ *
+ * The followings are the available columns in table 'feedback':
+ * @property integer $id_feedback
+ * @property string $komentar
+ * @property integer $id_regional
+ * @property string $nama_kegiatan
+ */
+class Feedback extends CActiveRecord
+{
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'feedback';
 	}
-?>
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('komentar, id_regional, nama_kegiatan', 'required'),
+			array('id_regional', 'numerical', 'integerOnly'=>true),
+			array('nama_kegiatan', 'length', 'max'=>64),
+			// The following rule is used by search().
+			// @todo Please remove those attributes that should not be searched.
+			array('id_feedback, komentar, id_regional, nama_kegiatan', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'id_feedback' => 'Id Feedback',
+			'komentar' => 'Komentar',
+			'id_regional' => 'Id Regional',
+			'nama_kegiatan' => 'Nama Kegiatan',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 *
+	 * Typical usecase:
+	 * - Initialize the model fields with values from filter form.
+	 * - Execute this method to get CActiveDataProvider instance which will filter
+	 * models according to data in model fields.
+	 * - Pass data provider to CGridView, CListView or any similar widget.
+	 *
+	 * @return CActiveDataProvider the data provider that can return the models
+	 * based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// @todo Please modify the following code to remove attributes that should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('id_feedback',$this->id_feedback);
+		$criteria->compare('komentar',$this->komentar,true);
+		$criteria->compare('id_regional',$this->id_regional);
+		$criteria->compare('nama_kegiatan',$this->nama_kegiatan,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+
+	/**
+	 * Returns the static model of the specified AR class.
+	 * Please note that you should have this exact method in all your CActiveRecord descendants!
+	 * @param string $className active record class name.
+	 * @return Feedback the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+	
+}
