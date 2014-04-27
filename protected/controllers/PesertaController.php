9bb7592aac58de7234656f62083eace87210c8b2
Herman <herman.wahyudi02@gmail.com>
2014-04-28 04:24:54 +0700

    Fix ubah profil/password, deadline, list peserta

--- protected/controllers/PesertaController.php
+++ protected/controllers/PesertaController.php
@@ -135,10 +135,8 @@ class PesertaController extends Controller
 		$id_user = Yii::app()->user->id;
 		$objRegional = Regional::model()->findByAttributes(array('id_user'=>$id_user));
 
-
 		$id_regional = $objRegional->id_regional;
-		$dataPeserta = Peserta::model()->findAllByAttributes(array('id_regional'=>$id_regional));
-		
+		//$dataPeserta = Peserta::model()->findAllByAttributes(array('id_regional'=>$id_regional));
 		
 		$model=new Peserta('search');
 		$model->unsetAttributes();  // clear any default values
