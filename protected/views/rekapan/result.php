702f3d6c05293f897c3de18682095d3390412b9d
Herman <herman.wahyudi02@gmail.com>
2014-05-07 20:23:49 +0700

    -- Grafik perbandingan rekapan

--- protected/views/rekapan/result.php
+++ protected/views/rekapan/result.php
@@ -5,40 +5,29 @@
 
 <div class="tag-box tag-box-v3">
               <div class="headline"><h1 class="text-center">Perbandingan Rekapan</h1></div>
-                    
-                        
-              <h3 class="text-center">Rekapan Periode Mei - Juli 2014</h3>
-			  <img src="<?php echo Yii::app()->request->baseUrl; ?>/assets/images/graph.png" />
-			  
-              <p class="text-center">
-					
-					<table id="myHtmlTable" border="2"><tbody>
-						<caption><h4>Detail</h4></caption><br>
-						<tr style="background-color:#E0FFFF"><th>Regional</th><th>Result</th></tr>
-						<tr>
-							<td align="left">Regional Jakarta 1 - Putra</td><td>10</td>
-						</tr>
-						<tr>
-							<td align="left">Regional Jakarta 2 - Putri</td><td>8</td>
-						</tr>
-						<tr>
-							<td align="left">Regional Jogjakarta</td><td>15</td>
-						</tr>
-						<tr>
-							<td align="left">Regional Bandung</td><td>12</td>
-						</tr>
-						</tbody>
-					</table>
-				
-					<script type="text/javascript">
-						$('#myHtmlTable').convertToFusionCharts({
-							swfPath: "siaek/charts/",
-							type: "MSColumn3D",
-							data: "#myHtmlTable",
-							dataFormat: "HTMLTable"
-						});
-					</script>
-			  </p>
+			  <?php
+						$this->widget('ext.highcharts.HighchartsWidget', array(
+					   'options'=>array(
+						  'title' => array('text' => 'Rekapan Antar-Regional PPSDMS'),
+						  'series' => array(
+							 array('type'=>'pie',
+								   'data' => array(array('Regional Jakarta 1',4),array('Regional Bandung',5),array('Regional Jogjakarta',2),)
+								  )
+						  ),
+						  'tooltip' => array(
+							'formatter' => 'js:function(){ return "<b>"+this.point.name+"> :"+this.y; }'
+						  ),
+						  'plotOptions'=>array('pie'=>(array(
+										'allowPointSelect'=>true,
+										'showInLegend'=>true,
+										'cursor'=>'pointer',
+									)
+								)                       
+							),
+						  'credits'=>array('enabled'=>false),
+					   )
+					));
+			 ?>
 			  <?php echo CHtml::link("Back", "comparerekapan"); ?>
               <div class="row clearfix">
               <br>
