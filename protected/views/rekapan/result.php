d8567737b3550c4b9db8c48ee9c9a3c35a1da27f
Herman <herman.wahyudi02@gmail.com>
2014-05-08 17:19:44 +0700

    -- Membuat Grafik dan melihat rekapan (Export to PDF)

--- protected/views/rekapan/result.php
+++ protected/views/rekapan/result.php
@@ -6,6 +6,26 @@
 <div class="tag-box tag-box-v3">
               <div class="headline"><h1 class="text-center">Perbandingan Rekapan</h1></div>
 			  <?php
+				 $this->Widget('ext.highcharts.HighchartsWidget', array(
+				   'options'=>array(
+					  'title' => array('text' => 'Rekapan Kegiatan Antar Regional PPSDMS <br><br><br>Agustus - Desember 2013'),
+					  'xAxis' => array(
+						 'categories' => array('Agustus', 'September', 'Oktober', 'November', 'Desember')
+					  ),
+					  'yAxis' => array(
+						 'title' => array('text' => 'Jumlah')
+					  ),
+					  'credits' => array('enabled' => false),
+					  'series' => array(
+						 array('name' => 'Regional 1 Putra Jakarta', 'data' => array(25, 26,27,28,33)),
+						 array('name' => 'Regional 1 Putri Jakarta', 'data' => array(30, 28, 25, 36,40)),
+						 array('name' => 'Regional Bandung', 'data' => array(17, 14,15,16,18)),
+						 array('name' => 'Regional Jogjakarta', 'data' => array(30, 28, 25, 26,28)),
+					  )
+				   )
+				));
+				?>
+			  <?php
 						$this->widget('ext.highcharts.HighchartsWidget', array(
 					   'options'=>array(
 						  'title' => array('text' => 'Rekapan Antar-Regional PPSDMS'),
@@ -28,7 +48,8 @@
 					   )
 					));
 			 ?>
-			  <?php echo CHtml::link("Back", "comparerekapan"); ?>
+			 
+			<?php echo CHtml::link("Back", "comparerekapan"); ?>
               <div class="row clearfix">
               <br>
               <div class="col-md-10 column">
