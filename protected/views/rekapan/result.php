cae09f9ea051a8250e0e191ffa4f018b0aa25366
Herman <herman.wahyudi02@gmail.com>
2014-05-08 23:02:01 +0700

    --Membuat grafik, export to Pdf, merapikan atribut list.

--- protected/views/rekapan/result.php
+++ protected/views/rekapan/result.php
@@ -13,7 +13,7 @@
 						 'categories' => array('Agustus', 'September', 'Oktober', 'November', 'Desember')
 					  ),
 					  'yAxis' => array(
-						 'title' => array('text' => 'Jumlah')
+						 'title' => array('text' => 'Persentase')
 					  ),
 					  'credits' => array('enabled' => false),
 					  'series' => array(
@@ -25,29 +25,7 @@
 				   )
 				));
 				?>
-			  <?php
-						$this->widget('ext.highcharts.HighchartsWidget', array(
-					   'options'=>array(
-						  'title' => array('text' => 'Rekapan Antar-Regional PPSDMS'),
-						  'series' => array(
-							 array('type'=>'pie',
-								   'data' => array(array('Regional Jakarta 1',4),array('Regional Bandung',5),array('Regional Jogjakarta',2),)
-								  )
-						  ),
-						  'tooltip' => array(
-							'formatter' => 'js:function(){ return "<b>"+this.point.name+"> :"+this.y; }'
-						  ),
-						  'plotOptions'=>array('pie'=>(array(
-										'allowPointSelect'=>true,
-										'showInLegend'=>true,
-										'cursor'=>'pointer',
-									)
-								)                       
-							),
-						  'credits'=>array('enabled'=>false),
-					   )
-					));
-			 ?>
+			  
 			 
 			<?php echo CHtml::link("Back", "comparerekapan"); ?>
               <div class="row clearfix">
